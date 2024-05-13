2a45587615926a754cc4154fee2924146d0900f8
cmukher1 <chiranjib.mukherjee@roitech.biz>
2024-05-14 02:15:09 +0530

    boiletplate

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <div className="card card-side bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+            alt="Movie"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Welcome to DaisyUI!</h2>
+          <p>This is boilerplate for Next with Daisy UI.</p>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
